70dd8696eaacc344c6e8d73c933f2e2464a0bdaf
Crainsver <cristobal21rc@gmail.com>
2026-09-01 13:21:25 -0400

    Mi primera pagina

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Selección de elementos
 const thumbs = document.querySelectorAll('.thumb');
 const mainImage = document.getElementById('mainImage');
+const mainDescription = document.getElementById('mainDescription');
 
+// Evento al seleccionar una imagen del catálogo
 thumbs.forEach(img => {
-    img.addEventListener('click' , () => {
-            mainImage.src=img.src.replace('/300/200','/600/400');
-    }
-    );
+    img.addEventListener('click', () => {
+        mainImage.src = img.src;
+        mainDescription.textContent = img.dataset.description;
+    });
 });
 
+// Lógica del Carrito de Compras, es bastante manual pero se entienda
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
@@ -15,20 +19,19 @@ const taskList = document.getElementById('taskList');
 addBtn.addEventListener('click', () => {
     const text = taskInput.value.trim();
 
-    if(text === ''){
-        alert('Debes escribir una tarea');
+    if (text === '') {
+        alert('Debes escribir un producto');
         return;
     }
 
-const li = document.createElement('li');
-
-const span = document.createElement('span');
-span.textContent = text;
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = text;
+    span.style.cursor = 'pointer';
 
     span.addEventListener('click', () => {
         span.classList.toggle('done');
     });
-
 
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Eliminar';
@@ -39,9 +42,7 @@ span.textContent = text;
 
     li.appendChild(span);
     li.appendChild(deleteBtn);
-
     taskList.appendChild(li);
 
     taskInput.value = '';
-
 });
